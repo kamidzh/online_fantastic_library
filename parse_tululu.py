2cd961097ac61369c8f85b2a4383e7263e1aa1a1
kamidzh <kamila.dzh09@gmail.com>
2024-11-02 14:00:47 +0300

    Проверила на редирект

--- parse_tululu.py
+++ parse_tululu.py
@@ -56,6 +56,7 @@ def get_category_books_url(start_page, end_page):
         url = f'https://tululu.org/l55/{page}'
         response = requests.get(url)
         response.raise_for_status()
+        check_for_redirect(response)
         soup = BeautifulSoup(response.text, 'lxml')
         books = soup.select('table.d_book')
         for book in books:
@@ -80,6 +81,7 @@ def main():
             print(url)
             response = requests.get(url)
             response.raise_for_status()
+            check_for_redirect(response)
             soup = BeautifulSoup(response.text, 'lxml')
             parameters = parse_book_page(soup, url)
             all_parameters.append(parameters)
